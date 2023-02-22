4502fd0f906d170c7264195597d64fdb5d2d6780
Andrew Beatty <andrew.beatty@atu.ie>
2023-02-22 10:03:08 +0000

    jupyter notebooks

---
+++ code/week05-data-structures/lecture/messingWithLists.py
@@ -0,0 +1,13 @@
+# messing with lists
+# Author: Andrew Beatty
+
+
+list = [2, 4, 5, 6]
+sum = 0
+for x in list:
+    sum += x
+    print (sum)
+
+# wrong way
+#for i in range(0,len(list)):
+#    print ("value of index ", i, " is ", list[i] )
