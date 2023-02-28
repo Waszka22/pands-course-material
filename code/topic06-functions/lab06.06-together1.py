b7c194765559dffdef0e2aec57967fefa100ab7c
Andrew Beatty <andrew.beatty@atu.ie>
2023-02-28 20:09:32 +0000

    fix bug in print

--- code/topic06-functions/lab06.06-together1.py
+++ code/topic06-functions/lab06.06-together1.py
@@ -33,7 +33,7 @@ def readModules():
 def displayModules(modules):
     print ("\tName   \tGrade")
     for module in modules:
-        print(f"\t{module["name"]}  \t{module["grade"]}") 
+        print(f"\t{module['name']}  \t{module['grade']}") 
 
 
 def doView(students):
