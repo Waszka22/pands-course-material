0e5050f0b47e9b2b282ce9476b8430b10a7027f1
Waszka22 <waszka04@gmail.com>
2023-03-30 23:51:22 +0100

    mywork

--- mywork/plottask.py
+++ mywork/plottask.py
@@ -1,11 +1,14 @@
 # Weekly Task 8
 # Author : Agnieszka Waszczuk
-# Ref https://www.w3schools.com/python/numpy/numpy_random_normal.asp
+# 
 # Write a program called plottask.py that displays:
 # a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
 # and a plot of the function  h(x)=x3 in the range [0, 10], 
 #on the one set of axes.
 #Some marks will be given for making the plot look nice (legend etc)
+
+
+# Work in progress!!!!
 
 from numpy import random
 x = random.normal(size=(0, 10000))
