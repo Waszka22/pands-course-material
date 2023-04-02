83936350b46763219b34e2dc44d775c856363eb9
Waszka22 <waszka04@gmail.com>
2023-04-03 00:25:25 +0100

    mywork

--- mywork/plottask.py
+++ mywork/plottask.py
@@ -6,14 +6,23 @@
 # and a plot of the function  h(x)=x3 in the range [0, 10], 
 #on the one set of axes.
 #Some marks will be given for making the plot look nice (legend etc)
-
+# ref https://www.w3schools.com/python/numpy/numpy_random_normal.asp
+#     https://www.w3schools.com/python/matplotlib_plotting.asp
 
 # Work in progress!!!!
+# generate normal distribution of a 1000 values 
+import numpy 
+#import matplotlib.pyplot as plot
 
-from numpy import random
-x = random.normal(size=(0, 10000))
+#from numpy import random
+#x = random.normal(size=(0, 10000))
 
-print(x)
+#x = random.normal(loc=5, scale=2, size=(0, 1000))
 
-mean = 5
-standard_deviation = 5
+#print(x)
+
+#mean = 5
+#standard_deviation = 5
+
+#import numpy
+
