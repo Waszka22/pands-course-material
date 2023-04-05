084a0b3da900fc02f77e565c75a6c5aeafdfc577
Waszka22 <waszka04@gmail.com>
2023-04-05 21:41:56 +0100

    mywork

--- mywork/mywork/helloword.py
+++ mywork/mywork/helloword.py
@@ -1,3 +1,11 @@
 # Weekly Task 01
+# Author : Agnieszka Waszczuk 
+# messing with inputs 
 
 print("HelloWord")
+name = input("Enter your name: ")
+print (f" Hello {name} \n Nice to meet you {name}")
+
+
+
+
